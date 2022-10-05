5e1b9715efd52ff787fe4bbea691734dad5cff08
Abdullah-sh <abo-ah-md@users.noreply.github.com>
2022-10-05 07:01:49 +0300

    adding server and api features

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+const projectData = {};
 
 // Require Express to run server and routes
 
+const express = require("express")
+const cors = require("cors")
+const bodyParser = require("body-parser");
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,33 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+app.use(cors())
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
+//HTTP handlers 
+app.get("/all",(req,res)=>{
+    res.send(projectData);
+});
+
+
+app.post("/newData",(req,res)=>{
+    
+    
+    projectData.temperature= req.body.temperature;
+    projectData.date= req.body.date;
+    projectData.userResponse= req.body.userResponse;
+
+    console.log(projectData);
+    res.send(projectData)
+
+
+    
+})
+
 // Setup Server
+const port = 3000;
+app.listen(port,()=>{
+    console.log(`server is running on port ${port} `);
+})
--- website/app.js
+++ website/app.js
@@ -1,5 +1,66 @@
 /* Global Variables */
+const apiKey = "&appid=9915480df1f9ed8ddae55f526b2e138d&units=imperial";
+const baseUrl = "https://api.openweathermap.org/data/2.5/weather?zip=";
+
+
+
+const generateBtn = document.querySelector("#generate");
 
 // Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+
+
+//----------------------
+const getApiData = async (baseUrl, zipInput, apiKey) => {
+  const res = await fetch(baseUrl + zipInput + apiKey);
+
+  try {
+    const response = await res.json();
+    const data = {temperature :data.main.temp}
+  } catch (error) {
+    console.log(error);
+  }
+};
+//----------------------
+const postData = async (url = "", data = {}) => {
+    
+  const serverResponse = await fetch(url, {
+    method: "POST",
+    credentials: "same-origin",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+
+  try {
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+//----------------------
+const getPost = (e) => {
+  const zipInput = document.querySelector("#zip").value;
+  const feelingsInput = document.querySelector("#feelings").value;
+
+  getApiData(baseUrl, zipInput, apiKey).then(postData("/newData", data));
+};
+
+//----------------------
+generateBtn.addEventListener("click", getPost);
+
+/*
+const retrieveData = async (url='') =>{ 
+    const request = await fetch(url);
+    try {
+    // Transform into JSON
+    const allData = await request.json()
+    }
+    catch(error) {
+      console.log("error", error);
+      // appropriately handle the error
+    }
+  }
+  */
