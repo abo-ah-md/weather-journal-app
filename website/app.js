f596208d5bf4a0a577918692c35e7da6a28ba401
Abdullah-sh <abo-ah-md@users.noreply.github.com>
2022-10-05 11:50:43 +0300

    final touches

--- website/app.js
+++ website/app.js
@@ -15,7 +15,7 @@ const contentEntry = document.querySelector("#content");
 // Create a new date instance dynamically with JS
 
 let d = new Date();
-let newDate = d.getMonth() + "." + d.getDate() + "." + d.getFullYear();
+let newDate = d.getMonth() + 1 + '.' + d.getDate() + '.' + d.getFullYear();
 
 //Function called by event listener
 
